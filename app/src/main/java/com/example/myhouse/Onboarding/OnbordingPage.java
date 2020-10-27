bf80ff6400139ea8791e74971ac54486752ef55e
JohnWick237 <55965251+loicsonwa@users.noreply.github.com>
2020-10-27 13:22:05 +0100

    Commit
    MyHouse Last version

---
+++ app/src/main/java/com/example/myhouse/Onboarding/OnbordingPage.java
@@ -0,0 +1,14 @@
+package com.example.myhouse.Onboarding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnbordingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onbording_page);
+    }
+}
